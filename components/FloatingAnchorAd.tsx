ab30320e18745f8150919faea86787dafaa4d0c4
angibeom0985-arch <angibeom0985@example.com>
2025-10-05 01:10:57 +0900

    Update anchor ad slot to 8116896499

--- components/FloatingAnchorAd.tsx
+++ components/FloatingAnchorAd.tsx
@@ -14,18 +14,13 @@ const FloatingAnchorAd: React.FC = () => {
     <div className="fixed bottom-0 left-0 right-0 z-40 bg-[#1A1A1A] shadow-lg">
       <div className="max-w-7xl mx-auto px-2 py-1">
         <center>
-          {/* 반응형: 모바일 320x50px / 데스크톱 728x90px */}
           <ins 
             className="adsbygoogle"
-            style={{ 
-              display: 'inline-block',
-              width: '100%',
-              maxWidth: '728px',
-              height: 'auto'
-            }}
+            style={{ display: 'block' }}
             data-ad-client="ca-pub-2686975437928535"
-            data-ad-slot="8015079861"
-            data-ad-format="horizontal"
+            data-ad-slot="8116896499"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
           />
         </center>
       </div>
